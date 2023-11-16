8929c47f47d19049288aa77a3c827ca17b876590
Pablo <48807058+PablooAjofrin@users.noreply.github.com>
2023-11-16 16:55:38 +0100

    Add files via upload

---
+++ resources/scenarios/function-jim-links-1700149293386.js
@@ -0,0 +1,312 @@
+(function(window, undefined) {
+
+  var jimLinks = {
+    "b4571a30-a504-44b5-9d2f-c20c8a504e86" : {
+      "Button_2" : [
+        "20dd865d-9fc1-415e-8648-7c4400a75d84"
+      ],
+      "Paragraph_1" : [
+        "20dd865d-9fc1-415e-8648-7c4400a75d84"
+      ]
+    },
+    "58b44310-44fd-4548-a59e-4f6afb614d61" : {
+      "Button_2" : [
+        "e045d30c-2c10-49df-a627-0d18118a798d"
+      ],
+      "Paragraph_1" : [
+        "e045d30c-2c10-49df-a627-0d18118a798d"
+      ]
+    },
+    "b774e602-0949-4049-8750-610650ce44f5" : {
+      "Paragraph_1" : [
+        "20dd865d-9fc1-415e-8648-7c4400a75d84"
+      ],
+      "Button_9" : [
+        "20dd865d-9fc1-415e-8648-7c4400a75d84"
+      ]
+    },
+    "20dd865d-9fc1-415e-8648-7c4400a75d84" : {
+      "Button_17" : [
+        "ef2533d2-0f1e-4d0b-b7b3-a7fa22341f0b"
+      ],
+      "Button_32" : [
+        "111104da-1411-4a8e-8b66-586c12ea375f"
+      ],
+      "Button_33" : [
+        "b4571a30-a504-44b5-9d2f-c20c8a504e86"
+      ],
+      "Button_2" : [
+        "b774e602-0949-4049-8750-610650ce44f5"
+      ],
+      "Button_5" : [
+        "51f83883-39f8-40ea-a653-8d4db39fb56c"
+      ],
+      "Button_3" : [
+        "cbf0e7f6-69ed-43c5-b97a-5fca7a799546"
+      ],
+      "Button_27" : [
+        "7bbec92c-f779-4bc2-9d85-ba237c06d714"
+      ],
+      "Paragraph_1" : [
+        "d12245cc-1680-458d-89dd-4f0d7fb22724"
+      ],
+      "Button_1" : [
+        "c062c09c-c8b4-4aae-948a-6dbecd518a6c"
+      ],
+      "Image_1" : [
+        "c062c09c-c8b4-4aae-948a-6dbecd518a6c"
+      ],
+      "Paragraph_2" : [
+        "d12245cc-1680-458d-89dd-4f0d7fb22724"
+      ],
+      "Paragraph_3" : [
+        "20dd865d-9fc1-415e-8648-7c4400a75d84"
+      ],
+      "Paragraph_4" : [
+        "98239622-f916-4f19-8757-22edeada94fd"
+      ]
+    },
+    "2b4d8da0-50e2-4551-9790-7a05236ee670" : {
+      "Button_3" : [
+        "e045d30c-2c10-49df-a627-0d18118a798d"
+      ],
+      "Paragraph_1" : [
+        "e045d30c-2c10-49df-a627-0d18118a798d"
+      ]
+    },
+    "51f83883-39f8-40ea-a653-8d4db39fb56c" : {
+      "Paragraph_1" : [
+        "20dd865d-9fc1-415e-8648-7c4400a75d84"
+      ],
+      "Button_1" : [
+        "20dd865d-9fc1-415e-8648-7c4400a75d84"
+      ]
+    },
+    "ef2533d2-0f1e-4d0b-b7b3-a7fa22341f0b" : {
+      "Paragraph_1" : [
+        "20dd865d-9fc1-415e-8648-7c4400a75d84"
+      ],
+      "Button_1" : [
+        "20dd865d-9fc1-415e-8648-7c4400a75d84"
+      ]
+    },
+    "7bbec92c-f779-4bc2-9d85-ba237c06d714" : {
+      "Paragraph_1" : [
+        "20dd865d-9fc1-415e-8648-7c4400a75d84"
+      ],
+      "Button_5" : [
+        "20dd865d-9fc1-415e-8648-7c4400a75d84"
+      ]
+    },
+    "530a28b9-f898-440e-a0c0-8bae0899bfff" : {
+      "Paragraph_1" : [
+        "e045d30c-2c10-49df-a627-0d18118a798d"
+      ],
+      "Button_1" : [
+        "e045d30c-2c10-49df-a627-0d18118a798d"
+      ]
+    },
+    "71abfe69-5213-4c74-922e-7bdc942cd5d6" : {
+      "Button_1" : [
+        "af4e7b50-4d85-4cc2-81b4-ac2572755dc0"
+      ],
+      "Image_1" : [
+        "af4e7b50-4d85-4cc2-81b4-ac2572755dc0"
+      ],
+      "Paragraph_2" : [
+        "a81ea37b-fd06-48a8-954c-5d1a02fa6cd5"
+      ],
+      "Paragraph_3" : [
+        "e045d30c-2c10-49df-a627-0d18118a798d"
+      ],
+      "Paragraph_4" : [
+        "71abfe69-5213-4c74-922e-7bdc942cd5d6"
+      ]
+    },
+    "c062c09c-c8b4-4aae-948a-6dbecd518a6c" : {
+      "Button_4" : [
+        "d12245cc-1680-458d-89dd-4f0d7fb22724"
+      ],
+      "Ellipse_1" : [
+        "d12245cc-1680-458d-89dd-4f0d7fb22724"
+      ],
+      "Image_4" : [
+        "d12245cc-1680-458d-89dd-4f0d7fb22724"
+      ]
+    },
+    "cbf0e7f6-69ed-43c5-b97a-5fca7a799546" : {
+      "Button_1" : [
+        "20dd865d-9fc1-415e-8648-7c4400a75d84"
+      ],
+      "Paragraph_1" : [
+        "20dd865d-9fc1-415e-8648-7c4400a75d84"
+      ]
+    },
+    "21af9ad3-4662-44cc-929d-f4732a3e990c" : {
+      "Button_1" : [
+        "e045d30c-2c10-49df-a627-0d18118a798d"
+      ],
+      "Paragraph_1" : [
+        "e045d30c-2c10-49df-a627-0d18118a798d"
+      ]
+    },
+    "3d696a26-97ff-43ae-8d5e-7db1f939d035" : {
+      "Paragraph_1" : [
+        "e045d30c-2c10-49df-a627-0d18118a798d"
+      ],
+      "Button_5" : [
+        "e045d30c-2c10-49df-a627-0d18118a798d"
+      ]
+    },
+    "111104da-1411-4a8e-8b66-586c12ea375f" : {
+      "Button_3" : [
+        "20dd865d-9fc1-415e-8648-7c4400a75d84"
+      ],
+      "Paragraph_1" : [
+        "20dd865d-9fc1-415e-8648-7c4400a75d84"
+      ]
+    },
+    "6af61c69-6624-4656-be87-0b394d8a8bc3" : {
+      "Paragraph_1" : [
+        "e045d30c-2c10-49df-a627-0d18118a798d"
+      ],
+      "Button_1" : [
+        "e045d30c-2c10-49df-a627-0d18118a798d"
+      ]
+    },
+    "e045d30c-2c10-49df-a627-0d18118a798d" : {
+      "Button_17" : [
+        "6af61c69-6624-4656-be87-0b394d8a8bc3"
+      ],
+      "Button_32" : [
+        "2b4d8da0-50e2-4551-9790-7a05236ee670"
+      ],
+      "Button_33" : [
+        "58b44310-44fd-4548-a59e-4f6afb614d61"
+      ],
+      "Button_2" : [
+        "45d479f1-5212-48c3-9528-dac47f740034"
+      ],
+      "Button_5" : [
+        "530a28b9-f898-440e-a0c0-8bae0899bfff"
+      ],
+      "Button_3" : [
+        "21af9ad3-4662-44cc-929d-f4732a3e990c"
+      ],
+      "Button_27" : [
+        "3d696a26-97ff-43ae-8d5e-7db1f939d035"
+      ],
+      "Paragraph_1" : [
+        "a81ea37b-fd06-48a8-954c-5d1a02fa6cd5"
+      ],
+      "Button_1" : [
+        "af4e7b50-4d85-4cc2-81b4-ac2572755dc0"
+      ],
+      "Image_1" : [
+        "af4e7b50-4d85-4cc2-81b4-ac2572755dc0"
+      ],
+      "Paragraph_2" : [
+        "a81ea37b-fd06-48a8-954c-5d1a02fa6cd5"
+      ],
+      "Paragraph_3" : [
+        "e045d30c-2c10-49df-a627-0d18118a798d"
+      ],
+      "Paragraph_4" : [
+        "71abfe69-5213-4c74-922e-7bdc942cd5d6"
+      ]
+    },
+    "a81ea37b-fd06-48a8-954c-5d1a02fa6cd5" : {
+      "Paragraph_2" : [
+        "a81ea37b-fd06-48a8-954c-5d1a02fa6cd5"
+      ],
+      "Paragraph_3" : [
+        "e045d30c-2c10-49df-a627-0d18118a798d"
+      ],
+      "Paragraph_4" : [
+        "71abfe69-5213-4c74-922e-7bdc942cd5d6"
+      ],
+      "Button_13" : [
+        "e045d30c-2c10-49df-a627-0d18118a798d"
+      ],
+      "Rectangle_5" : [
+        "d12245cc-1680-458d-89dd-4f0d7fb22724"
+      ],
+      "Button_1" : [
+        "e045d30c-2c10-49df-a627-0d18118a798d"
+      ],
+      "Button_3" : [
+        "af4e7b50-4d85-4cc2-81b4-ac2572755dc0"
+      ],
+      "Image_22" : [
+        "af4e7b50-4d85-4cc2-81b4-ac2572755dc0"
+      ]
+    },
+    "45d479f1-5212-48c3-9528-dac47f740034" : {
+      "Button_9" : [
+        "e045d30c-2c10-49df-a627-0d18118a798d"
+      ],
+      "Paragraph_61" : [
+        "e045d30c-2c10-49df-a627-0d18118a798d"
+      ]
+    },
+    "98239622-f916-4f19-8757-22edeada94fd" : {
+      "Paragraph_2" : [
+        "d12245cc-1680-458d-89dd-4f0d7fb22724"
+      ],
+      "Paragraph_3" : [
+        "20dd865d-9fc1-415e-8648-7c4400a75d84"
+      ],
+      "Paragraph_4" : [
+        "98239622-f916-4f19-8757-22edeada94fd"
+      ],
+      "Button_1" : [
+        "c062c09c-c8b4-4aae-948a-6dbecd518a6c"
+      ],
+      "Image_7" : [
+        "c062c09c-c8b4-4aae-948a-6dbecd518a6c"
+      ]
+    },
+    "af4e7b50-4d85-4cc2-81b4-ac2572755dc0" : {
+      "Button_4" : [
+        "a81ea37b-fd06-48a8-954c-5d1a02fa6cd5"
+      ],
+      "Ellipse_1" : [
+        "af4e7b50-4d85-4cc2-81b4-ac2572755dc0"
+      ],
+      "Image_4" : [
+        "a81ea37b-fd06-48a8-954c-5d1a02fa6cd5"
+      ]
+    },
+    "d12245cc-1680-458d-89dd-4f0d7fb22724" : {
+      "Paragraph_2" : [
+        "d12245cc-1680-458d-89dd-4f0d7fb22724"
+      ],
+      "Paragraph_3" : [
+        "20dd865d-9fc1-415e-8648-7c4400a75d84"
+      ],
+      "Paragraph_4" : [
+        "98239622-f916-4f19-8757-22edeada94fd"
+      ],
+      "Button_10" : [
+        "c062c09c-c8b4-4aae-948a-6dbecd518a6c"
+      ],
+      "Image_2" : [
+        "c062c09c-c8b4-4aae-948a-6dbecd518a6c"
+      ],
+      "Button_13" : [
+        "20dd865d-9fc1-415e-8648-7c4400a75d84"
+      ],
+      "Rectangle_5" : [
+        "d12245cc-1680-458d-89dd-4f0d7fb22724"
+      ],
+      "Rectangle_6" : [
+        "a81ea37b-fd06-48a8-954c-5d1a02fa6cd5"
+      ],
+      "Button_1" : [
+        "20dd865d-9fc1-415e-8648-7c4400a75d84"
+      ]
+    }    
+  }
+
+  window.jimLinks = jimLinks;
+})(window);
