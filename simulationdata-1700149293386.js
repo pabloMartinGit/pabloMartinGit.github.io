56088542c4458856bb144234243a36e274004958
Pablo <48807058+PablooAjofrin@users.noreply.github.com>
2023-11-16 16:50:35 +0100

    Add files via upload

---
+++ simulationdata-1700149293386.js
@@ -0,0 +1,12 @@
+function initData() {
+  jimData.variables["Usuario"] = "User";
+  jimData.variables["Leccion 3-1"] = "0";
+  jimData.variables["Leccion 2-2"] = "0";
+  jimData.variables["Leccion 1-3"] = "";
+  jimData.variables["Leccion 2-1"] = "0";
+  jimData.variables["Lección 1-4"] = "0";
+  jimData.variables["Inicio sesión"] = "0";
+  jimData.variables["Lección 1-2"] = "0";
+  jimData.variables["Lección 1-1"] = "0";
+  jimData.isInitialized = true;
+}
